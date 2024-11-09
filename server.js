9f581013c336d1ca5e0b765512becc79a7cefaa1
SLRU0 <119642591+SLRU0@users.noreply.github.com>
2024-11-09 15:05:07 +0800

    continue to edit the login page

--- server.js
+++ server.js
@@ -2,12 +2,26 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8099;
 
-app.set('view engine', 'ejs');
-
-app.get('/', function (req, res) {
-    res.send("HI")
+//app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+/**
+ * This is the home page
+ */
+app.get('/', (req, res)=> {
+    /**
+     * this is variable is rendered to the website.
+     * @type {{password: string, title: string, account: string}}
+     */
+    let data = {
+        title:'Home',
+        account:'Account',
+        password:'Password',
+    }
+    res.render('index',{data:data});
 })
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+    console.log(`Listening on port http://localhost:${port}`);
 })
